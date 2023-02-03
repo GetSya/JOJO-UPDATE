d3febdd0cf51c2f187ddd9873b98ccebb7e5929a
Arasya Rafi <94653668+GetSya@users.noreply.github.com>
2023-02-03 16:48:18 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 
 // setting 
 global.firtsChat = true //send firts message to new user
-global.autoread = false // auto read message
-global.antidelete = true // anti delete
-global.antiviewonce = true // anti view once
+global.autoread = true // auto read message
+global.antidelete = false // anti delete
+global.antiviewonce = false // anti view once
 global.autobio = false //auto ganti bio
 global.anticall = true //anti call 
 global.userRegister = false
@@ -37,24 +37,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 // Other
 global.botname = "Alphabot-Mdོ" //namabot kalian
 global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.myweb = "https://myprofile.syaweb.repl.co" //bebas asal jan hapus
+global.youtube = "https://www.youtube.com/@jojochannel2488" //bebas asal jan hapus
+global.myweb2 = "https://dosq.pplg.repl.co" //bebas
+global.email = "arasyarafi02@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.premium = ['6281319944917'] //premium user
+global.owner = ["6281319944917", "6281319944917"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6281319944917" // nomor wa kalian
+global.ownernomerr = "+6281319944917" //nmr wa kalian
+global.thumbnail = "./image/bot.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi2.png" // foto donasi di folder image
+global.background_welcome = "https://t4.ftcdn.net/jpg/04/20/83/53/360_F_420835367_fFscB0EF5RiSMmcPLtZIxXQk9DTdTCvy.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = 'My Sticker' //sticker wm ubah
+global.author = 'Sticker Saya' //sticker wm ganti nama kalian
 global.sessionName = 'session'
-global.typemenu = 'document'
+global.typemenu = 'session'
 global.multiplier = 69
 global.limitawal = {
    premium: "10000",
@@ -126,7 +126,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['', '!', '.', '🐦', '🐤', '🗿']
 global.sp = '⭔'
 global.mess = {
-   sukses: '🤗Done, Oke Desu~',
+   sukses: 'Done',
    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
@@ -134,7 +134,7 @@ global.mess = {
    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
    bot: 'Fitur Khusus Pengguna Nomor Bot',
    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-   wait: '⏳ Sedang Di Proses',
+   wait: '_Tunggu Sebentar Yah Kak!\nSedang Jojo Proses Nih_',
    lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
    example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
